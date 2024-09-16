56eef7892d7a015de40b7b015ebbaf40fbb384ed
Nurbek333 <nurbekuktamov333@gmail.com>
2024-09-16 22:59:46 +0500

    com

--- states/reklama.py
+++ states/reklama.py
@@ -2,3 +2,10 @@ from aiogram.fsm.state import State, StatesGroup
 
 class Adverts(StatesGroup):
     adverts = State()
+
+
+class SendState(StatesGroup):
+    ask = State()
+    time = State()
+    confirm = State()
+    # send = State()
